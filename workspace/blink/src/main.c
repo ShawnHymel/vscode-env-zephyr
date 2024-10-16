608037b3d52ec86304c9e91ed8a47630954faed1
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2024-10-15 18:11:50 -0600

    Seems to work on linux/amd64

--- workspace/blink/src/main.c
+++ workspace/blink/src/main.c
@@ -41,7 +41,7 @@ int main(void)
 		}
 
 		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		printk("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
